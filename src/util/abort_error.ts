b5cae0e9bd26b80e95151dffef140d46d441374c
Yasuaki KAWAKAMI <yasuaki.kawakami@geolonia.com>
2025-09-17 15:31:37 +0900

    JSDocを翻訳

--- src/util/abort_error.ts
+++ src/util/abort_error.ts
@@ -1,20 +1,20 @@
 /**
- * An error message to use when an operation is aborted
+ * 操作が中止された時に使用するエラーメッセージ
  */
 export const ABORT_ERROR = 'AbortError';
 
 /**
- * Check if an error is an abort error
- * @param error - An error object
- * @returns - true if the error is an abort error
+ * エラーが中止エラーかどうかをチェックする
+ * @param error - エラーオブジェクト
+ * @returns - エラーが中止エラーの場合はtrue
  */
 export function isAbortError(error: Error): boolean {
     return error.message === ABORT_ERROR;
 }
 
 /**
- * Use this when you need to create an abort error.
- * @returns An error object with the message "AbortError"
+ * 中止エラーを作成する必要がある場合に使用する。
+ * @returns "AbortError"メッセージを持つエラーオブジェクト
  */
 export function createAbortError(): Error {
     return new Error(ABORT_ERROR);
